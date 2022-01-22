5ce59e8fd6a3a2b93e1b20e4565d779d37d646aa
moin <moin.uddin@technovista.com.bd>
2022-01-22 10:58:14 +0600

    woking with hooks

--- src/Main.js
+++ src/Main.js
@@ -1,18 +1,37 @@
 import React from 'react';
+import { useState } from "react";
 
 function Main(props) {
     const moin = "name";
     const job = "rap";
 
+    const [name, setName] = useState();
+
     const handleNameChange = () => {
     const names = ["moin", "happy", "khushi"];
     const int = Math.floor(Math.random() * 3);
-    return names[int];
+    return setName(names[int]);
+    };
+
+    const handleMessage = () => {
+        alert('hello');
+    }
+    const handleMessage1 = (myname) => {
+      alert(myname);
     };
     return (
       <div>
         <header>
-          <p>{handleNameChange()}</p>
+          <p>{name}</p>
+          {/* <p>{handleNameChange}</p> */}
+          <button onClick={handleNameChange}>Click Me!</button>
+          <button
+            onClick={() => {
+              handleMessage1("myname");
+            }}
+          >
+            Once More Click Me!
+          </button>
         </header>
       </div>
     );
